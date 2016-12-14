854b73e46a1037b508bc3b5635d402509809113f
Jones <jonesprabu@gmail.com>
2016-12-14 19:38:56 +0530

    Components added

---
+++ src/app/item-list/item-list.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { ItemListComponent } from './item-list.component';
+
+describe('Component: ItemList', () => {
+  it('should create an instance', () => {
+    let component = new ItemListComponent();
+    expect(component).toBeTruthy();
+  });
+});
